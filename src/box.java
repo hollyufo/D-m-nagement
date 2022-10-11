a027b7e42955fe61a9d01c894511dd8362d8deaa
holly ufo <imranchaibi1@gmail.com>
2022-10-11 14:10:18 +0100

    creating box class

---
+++ src/box.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class box {
+    // creating arraylist to store boxes
+    ArrayList<box> boxes = new ArrayList<box>();
+    // method to create box
+    public void createBox(){
+        // creating box
+        box box = new box();
+        // adding box to arraylist
+        boxes.add(box);
+    }
+}
